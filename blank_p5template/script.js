a2d1528ed529c10526ecb02bfc985c02c76b016b
tal9565 <113409796+tal9565@users.noreply.github.com>
2022-11-25 20:22:22 +0800

    my code

---
+++ blank_p5template/script.js
@@ -0,0 +1,12 @@
+console.log("script has loaded")
+
+function setup(){
+
+   let cnv = createCanvas(200, 200);
+   cnv.parent("canvasContainer");
+    background(100, 170, 210);
+}
+
+function draw(){
+
+}
